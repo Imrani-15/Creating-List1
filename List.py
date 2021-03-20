b96d937271497ccfe2292a78681e9df9c553d682
Imrani-15 <startcareer.15@gmail.com>
2021-03-20 12:03:05 +0530

    Creating List

---
+++ List.py
@@ -0,0 +1,24 @@
+# Pratice on List Methods
+
+# Creating List
+List = []
+print("Blanck List:")
+print(List)
+
+# Creating list of numbers
+List1 = [10,20,30]
+print("\n List of numbers:")
+print(List1)
+
+# Creating a List of strings and accessing
+# using index
+List2 = ["apple","banana", "grapes"]
+print("\n items of list")
+print(List2[0])
+print(List2[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List3 = [['PG', 'College'], ['school']]
+print("\n Multidemensional List:")
+print(List3)
